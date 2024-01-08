78c6f395194066b4f4807193508d0e8566fb54f7
stebenpe <stevenpennock7@gmail.com>
2024-01-08 13:23:10 +0100

    cleanup

--- host/run-rviz/start-rviz.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export DISPLAY=:10
-
-source /opt/ros/humble/setup.bash
-
-source /home/steven/Desktop/docker/ubuntu_minimal_ros2_humble/Data/ros2_ws/Omron_AMR_ROS2/install/setup.bash
-
-ros2 launch amr_visualisation display.launch.py
